11c71162ce62c5df904abab840ab9ddac81877d4
linhowell <lin.howell18@gmail.com>
2022-04-01 09:30:58 +0800

    血量顯示

--- game/Source/resource.h
+++ game/Source/resource.h
@@ -61,6 +61,7 @@
 #define IDB_BITMAP18                    179
 #define IDB_BITMAP19                    180
 #define IDB_BITMAP20                    181
+#define IDB_BITMAP21                    182
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -73,7 +74,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        182
+#define _APS_NEXT_RESOURCE_VALUE        183
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
