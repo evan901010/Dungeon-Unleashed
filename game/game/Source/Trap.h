c7bff54351938a2e9548d28f2442e42b097917e7
evan901010 <97020557+evan901010@users.noreply.github.com>
2022-06-24 00:57:17 +0800

    update final version

--- game/game/Source/Trap.h
+++ game/game/Source/Trap.h
@@ -1,18 +1,11 @@
 #pragma once
-
 #include "CSword.h"
-
 namespace game_framework {
-	/////////////////////////////////////////////////////////////////////////////
-	// 這個class提供繞圈圈的球
-	// 看懂就可以改寫成自己的程式了
-	/////////////////////////////////////////////////////////////////////////////
-
 	class Trap
 	{
 	public:
 		Trap();
-		bool HitEraser(CEraser *eraser);						// 是否碰到擦子
+		bool HitEraser(CEraser *eraser);						
 		bool HitCSword(CSword *sword);
 		bool HitOthers(Trap *ball);
 		bool IsAlive();
@@ -20,9 +13,9 @@ namespace game_framework {
 		void LoadBitmap();										// 載入圖形
 		void OnMove();											// 移動
 		void OnShow();											// 將圖形貼到畫面
-		void SetXY(int nx, int ny);								// 設定圓心的座標
+		void SetXY(int nx, int ny);								
 		void SetIsAlive(bool alive);							// 設定是否活著
-		void SetDelay(int d);									// 設定旋轉的速度
+		void SetDelay(int d);									
 		void getxy(int x2, int y2);
 		void forward();
 		void back();
@@ -33,13 +26,13 @@ namespace game_framework {
 		int GetY2();
 		bool ittouch;
 	protected:
-		CMovingBitmap bmp;			// 球的圖
-		CMovingBitmap bmp_center;	// 圓心的圖			
-		int x, y;					// 圓心的座標
+		CMovingBitmap bmp;			
+		CMovingBitmap bmp_center;	
+		int x, y;					
 		int x1, y1;
-		int dx, dy;					// 球距離圓心的位移量
+		int dx, dy;					
 		int inx, iny;
-		int index;					// 球的「角度」，0-17表示0-360度
+		int index;					
 		int delay_counter;			// 調整旋轉速度的計數器
 		int delay;					// 旋轉的速度
 		bool is_alive;				// 是否活著
---
+++ game/game/game/Source/mygame.h
@@ -0,0 +1,170 @@
+/*
+ * mygame.h: 本檔案儲遊戲本身的class的interface
+ * Copyright (C) 2002-2008 Woei-Kae Chen <wkc@csie.ntut.edu.tw>
+ *
+ * This file is part of game, a free game development framework for windows.
+ *
+ * game is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * game is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *	 2004-03-02 V4.0
+ *      1. Add CGameStateInit, CGameStateRun, and CGameStateOver to
+ *         demonstrate the use of states.
+ *   2005-09-13
+ *      Rewrite the codes for CBall and CEraser.
+ *   2005-09-20 V4.2Beta1.
+ *   2005-09-29 V4.2Beta2.
+ *   2006-02-08 V4.2
+ *      1. Rename OnInitialUpdate() -> OnInit().
+ *      2. Replace AUDIO_CANYON as AUDIO_NTUT.
+ *      3. Add help bitmap to CGameStateRun.
+ *   2006-09-09 V4.3
+ *      1. Rename Move() and Show() as OnMove and OnShow() to emphasize that they are
+ *         event driven.
+ *   2008-02-15 V4.4
+ *      1. Add namespace game_framework.
+ *      2. Replace the demonstration of animation as a new bouncing ball.
+ *      3. Use ShowInitProgress(percent) to display loading progress.
+*/
+
+#include "CEraser.h"
+#include "CBall.h"
+#include "CBouncingBall.h"
+#include "orc.h"
+#include "trap.h"
+#include "Boss.h"
+#include "Chest.h"
+#include "Zombie.h"
+
+namespace game_framework {
+	/////////////////////////////////////////////////////////////////////////////
+	// Constants
+	/////////////////////////////////////////////////////////////////////////////
+
+	enum AUDIO_ID {				// 定義各種音效的編號
+		AUDIO_DING,				// 0
+		AUDIO_LAKE,				// 1
+		AUDIO_NTUT,			// 2
+		AUDIO_SWORD_HIT	,	//3
+		AUDIO_BOSS_HIT,
+		AUDIO_BOSS_BATTLE,
+		AUDIO_CHEST
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// 這個class為遊戲的遊戲開頭畫面物件
+	// 每個Member function的Implementation都要弄懂
+	/////////////////////////////////////////////////////////////////////////////
+
+	class CGameStateInit : public CGameState {
+	public:
+		CGameStateInit(CGame *g);
+		void OnInit();  								// 遊戲的初值及圖形設定
+		void OnBeginState();							// 設定每次重玩所需的變數
+		void OnKeyUp(UINT, UINT, UINT); 				// 處理鍵盤Up的動作
+		void OnLButtonDown(UINT nFlags, CPoint point);  // 處理滑鼠的動作
+	protected:
+		void OnShow();									// 顯示這個狀態的遊戲畫面
+	private:
+		CMovingBitmap logo;								// csie的logo
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// 這個class為遊戲的遊戲執行物件，主要的遊戲程式都在這裡
+	// 每個Member function的Implementation都要弄懂
+	/////////////////////////////////////////////////////////////////////////////
+
+
+	class CPractice {
+	public:
+		CPractice();
+		void LoadBitmap();
+		void OnMove();
+		void OnShow();
+	private:
+		CMovingBitmap pic;
+		int x, y;
+		
+	};
+	class CGameStateRun : public CGameState {
+	public:
+		CGameStateRun(CGame *g);
+		~CGameStateRun();
+		void newlevel();
+		void OnBeginState();							// 設定每次重玩所需的變數
+		void OnInit();  								// 遊戲的初值及圖形設定
+		void OnKeyDown(UINT, UINT, UINT);
+		void OnKeyUp(UINT, UINT, UINT);
+		void OnLButtonDown(UINT nFlags, CPoint point);  // 處理滑鼠的動作
+		void OnLButtonUp(UINT nFlags, CPoint point);	// 處理滑鼠的動作
+		void OnMouseMove(UINT nFlags, CPoint point);	// 處理滑鼠的動作 
+		void OnRButtonDown(UINT nFlags, CPoint point);  // 處理滑鼠的動作
+		void OnRButtonUp(UINT nFlags, CPoint point);	// 處理滑鼠的動作
+	protected:
+		void OnMove();									// 移動遊戲元素
+		void OnShow();									// 顯示這個狀態的遊戲畫面
+	private:
+		int enemyconst ,totalenemy, totalboss, totaltrap, totalchest, totalzombie;
+		int level;
+		CPractice c_practice;
+		CMovingBitmap pratice;
+		int picX, picY;
+		CMovingBitmap practice;
+		const int		NUMBALLS;	// 球的總數
+		CMovingBitmap	background;	// 背景圖
+		CMovingBitmap	help;		// 說明圖
+		CBall			*ball;		// 球的陣列
+		CMovingBitmap	corner;		// 角落圖
+		CEraser			eraser;		// 拍子
+		CSword			sword;
+		CInteger		hits_left;	// 剩下的撞擊數
+		CInteger        boss_blood ;
+		Trap *trap;
+		Chest *chest;
+		Zombie *zombie;
+		CGamemap   map;		// 反覆彈跳的球
+		orc orc;
+		Boss *boss;
+		CMovingBitmap boss_blood1;
+		CMovingBitmap boss_blood2;
+		CMovingBitmap boss_blood3;
+		CMovingBitmap boss_blood4;
+		CMovingBitmap boss_blood5;
+		CMovingBitmap boss_blood6;
+		CMovingBitmap boss_blood7;
+		CMovingBitmap boss_blood8;
+		CMovingBitmap boss_blood9;
+		CMovingBitmap boss_blood10;
+		CMovingBitmap boss_blood11;
+		CMovingBitmap boss_blood12;
+	};
+	
+	/////////////////////////////////////////////////////////////////////////////
+	// 這個class為遊戲的結束狀態(Game Over)
+	// 每個Member function的Implementation都要弄懂
+	/////////////////////////////////////////////////////////////////////////////
+
+	class CGameStateOver : public CGameState {
+	public:
+		CGameStateOver(CGame *g);
+		void OnBeginState();							// 設定每次重玩所需的變數
+		void OnInit();
+	protected:
+		void OnMove();									// 移動遊戲元素
+		void OnShow();									// 顯示這個狀態的遊戲畫面
+	private:
+		int counter;	// 倒數之計數器
+	};
+
+}
